eebb66d1ffce49f28c02c987e17662635cf22ca6
Satyam Bharti <73614498+Sattushivaa@users.noreply.github.com>
2021-04-02 10:56:28 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
 console.log('Hello World!');
 var xhr = new XMLHttpRequest();
 var input = document.getElementById('file');
+var errorField = document.getElementById('error');
 
 function getData(){
-xhr.open('GET','Satyam.txt',true);
-
-
-
-
+xhr.open('GET',input.value,true);
 xhr.onload = function(){
   document.body.textContent = xhr.response;
 }
 xhr.send();
 xhr.onerror = function(error){
   console.log(error);
+  errorField.innerText = error;
 }
 }
 
 input.onchange = function(e){
   console.log(e.target.value);
+  errorField.innerText += ' : ' + e.target.value ;
 }
